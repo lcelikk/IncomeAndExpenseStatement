0a94c66207018142f741689c5f827893a2ea8498
lcelikk <celiklutfullah58@gmail.com>
2026-05-22 19:14:19 +0300

    Proje Dosyası Yüklendi

---
+++ Gelir_Gider/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace GelirGiderTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.txtAciklama = new System.Windows.Forms.TextBox();
+            this.dtTarih = new System.Windows.Forms.DateTimePicker();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.lblToplamGelir = new System.Windows.Forms.Label();
+            this.lblToplamGider = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(180, 68);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(692, 355);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Location = new System.Drawing.Point(16, 95);
+            this.cmbTur.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(136, 31);
+            this.cmbTur.TabIndex = 1;
+            this.cmbTur.SelectedIndexChanged += new System.EventHandler(this.cmbTur_SelectedIndexChanged);
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(16, 224);
+            this.cmbKategori.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(136, 31);
+            this.cmbKategori.TabIndex = 2;
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(16, 320);
+            this.txtTutar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(112, 30);
+            this.txtTutar.TabIndex = 3;
+            // 
+            // txtAciklama
+            // 
+            this.txtAciklama.Location = new System.Drawing.Point(16, 445);
+            this.txtAciklama.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAciklama.Name = "txtAciklama";
+            this.txtAciklama.Size = new System.Drawing.Size(112, 30);
+            this.txtAciklama.TabIndex = 4;
+            // 
+            // dtTarih
+            // 
+            this.dtTarih.Location = new System.Drawing.Point(2, 616);
+            this.dtTarih.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtTarih.Name = "dtTarih";
+            this.dtTarih.Size = new System.Drawing.Size(224, 30);
+            this.dtTarih.TabIndex = 5;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.LightGreen;
+            this.btnEkle.Location = new System.Drawing.Point(274, 446);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(84, 32);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.LightCoral;
+            this.btnSil.Location = new System.Drawing.Point(413, 445);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(84, 32);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnListele.Location = new System.Drawing.Point(550, 445);
+            this.btnListele.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(84, 32);
+            this.btnListele.TabIndex = 8;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // lblToplamGelir
+            // 
+            this.lblToplamGelir.AutoSize = true;
+            this.lblToplamGelir.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblToplamGelir.Location = new System.Drawing.Point(270, 571);
+            this.lblToplamGelir.Name = "lblToplamGelir";
+            this.lblToplamGelir.Size = new System.Drawing.Size(55, 23);
+            this.lblToplamGelir.TabIndex = 9;
+            this.lblToplamGelir.Text = "label1";
+            // 
+            // lblToplamGider
+            // 
+            this.lblToplamGider.AutoSize = true;
+            this.lblToplamGider.Location = new System.Drawing.Point(522, 571);
+            this.lblToplamGider.Name = "lblToplamGider";
+            this.lblToplamGider.Size = new System.Drawing.Size(55, 23);
+            this.lblToplamGider.TabIndex = 10;
+            this.lblToplamGider.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(360, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Finansal Takip";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Tür";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Kategori";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 23);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Tutar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 23);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Açıklama";
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Location = new System.Drawing.Point(640, 622);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(55, 23);
+            this.lblBakiye.TabIndex = 16;
+            this.lblBakiye.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(896, 647);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblToplamGider);
+            this.Controls.Add(this.lblToplamGelir);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dtTarih);
+            this.Controls.Add(this.txtAciklama);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.cmbTur);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Finans Takip";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.TextBox txtAciklama;
+        private System.Windows.Forms.DateTimePicker dtTarih;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Label lblToplamGelir;
+        private System.Windows.Forms.Label lblToplamGider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblBakiye;
+    }
+}
+
---
+++ Gelir_Gider/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GelirGiderTakip
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection(
+            "Server=DESKTOP-2T9GGTI\\SQLEXPRESS;Database=GelirGiderDB;Trusted_Connection=True;"
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbTur.Items.Clear();
+
+            cmbTur.Items.Add("Gelir");
+
+            cmbTur.Items.Add("Gider");
+            
+
+
+            Listele();
+            ToplamlariHesapla();
+
+        }
+
+        void KategorileriGetir()
+        {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT * FROM Kategoriler",
+                baglanti
+            );
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            cmbKategori.DisplayMember = "KategoriAdi";
+            cmbKategori.ValueMember = "KategoriID";
+            cmbKategori.DataSource = dt;
+        }
+
+        void Listele()
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(
+                    @"SELECT 
+                I.IslemID,
+                I.Tur,
+                I.Tutar,
+                K.KategoriAdi,
+                I.Tarih,
+                I.Aciklama
+              FROM Islemler I
+              LEFT JOIN Kategoriler K
+              ON I.KategoriID = K.KategoriID",
+                    baglanti
+                );
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Listeleme hatası: " + ex.Message);
+            }
+        }
+
+        void ToplamlariHesapla()
+        {
+            baglanti.Open();
+
+            SqlCommand cmdGelir = new SqlCommand(
+                "SELECT ISNULL(SUM(Tutar),0) FROM Islemler WHERE Tur='Gelir'",
+                baglanti
+            );
+
+            SqlCommand cmdGider = new SqlCommand(
+                "SELECT ISNULL(SUM(Tutar),0) FROM Islemler WHERE Tur='Gider'",
+                baglanti
+            );
+
+            lblToplamGelir.Text =
+                "Toplam Gelir: " + cmdGelir.ExecuteScalar().ToString() + " TL";
+
+            lblToplamGider.Text =
+                "Toplam Gider: " + cmdGider.ExecuteScalar().ToString() + " TL";
+            decimal bakiye = (decimal)cmdGelir.ExecuteScalar() - (decimal)cmdGider.ExecuteScalar();
+            lblBakiye.Text = "Güncel Bakiye: " + bakiye + " TL";
+
+            baglanti.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+          
+            try
+            {
+                // 1. Boş alan kontrolü
+                if (string.IsNullOrWhiteSpace(txtTutar.Text) ||
+                    string.IsNullOrWhiteSpace(cmbTur.Text) ||
+                    cmbKategori.SelectedValue == null)
+                {
+                    MessageBox.Show("Lütfen tüm alanları doldurun!");
+                    return;
+                }
+
+                // 2. Tutar kontrolü (decimal güvenli parse)
+                if (!decimal.TryParse(txtTutar.Text, out decimal tutar))
+                {
+                    MessageBox.Show("Tutar geçerli bir sayı olmalı!");
+                    return;
+                }
+
+                // 3. Bağlantı aç
+                if (baglanti.State == ConnectionState.Closed)
+                    baglanti.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    @"INSERT INTO Islemler
+              (Tur, Tutar, KategoriID, Tarih, Aciklama)
+              VALUES
+              (@tur, @tutar, @kategori, @tarih, @aciklama)",
+                    baglanti
+                );
+
+                cmd.Parameters.AddWithValue("@tur", cmbTur.Text);
+                cmd.Parameters.AddWithValue("@tutar", tutar);
+                cmd.Parameters.AddWithValue("@kategori", cmbKategori.SelectedValue);
+                cmd.Parameters.AddWithValue("@tarih", dtTarih.Value);
+                cmd.Parameters.AddWithValue("@aciklama",
+                    string.IsNullOrWhiteSpace(txtAciklama.Text) ? "" : txtAciklama.Text
+                );
+
+                cmd.ExecuteNonQuery();
+
+                baglanti.Close();
+
+                MessageBox.Show("İşlem başarıyla eklendi!");
+
+                // 4. Ekranı güncelle
+                Listele();
+                ToplamlariHesapla();
+            }
+            catch (Exception ex)
+            {
+                if (baglanti.State == ConnectionState.Open)
+                    baglanti.Close();
+
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+        
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            Listele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(
+                dataGridView1.CurrentRow.Cells["IslemID"].Value
+            );
+
+            baglanti.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "DELETE FROM Islemler WHERE IslemID=@id",
+                baglanti
+            );
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+
+            baglanti.Close();
+
+            MessageBox.Show("Kayıt Silindi");
+
+            Listele();
+            ToplamlariHesapla();
+        }
+
+        private void btnEkle_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void cmbTur_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTur.SelectedIndex == -1) return;
+
+            cmbKategori.DataSource = null;
+            cmbKategori.Items.Clear();
+
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT KategoriID, KategoriAdi FROM Kategoriler WHERE Tur=@tur",
+                baglanti
+            );
+
+            da.SelectCommand.Parameters.AddWithValue("@tur", cmbTur.Text);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            cmbKategori.DisplayMember = "KategoriAdi";
+            cmbKategori.ValueMember = "KategoriID";
+
+            cmbKategori.DataSource = dt;
+        }
+
+    }
+}
